50e6b7d9952a5eb76758df31748d546258dadcc5
Walters Mitch <mitchell_walters@Walterss-MacBook-Pro.local>
2016-01-10 15:28:34 -0800

    finished up the two player implementation.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,7 @@
 
 class Board
 
-  attr_accessor :board_state, :x_player, :o_player
+  attr_accessor :board_state, :x_player, :o_player, :winner, :game_has_been_won, :current_turn, :false
   attr_reader :winning_combos
 
   def initialize
@@ -9,6 +9,11 @@ class Board
     @winning_combos = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3] ]
     @x_player = Player.new("x")
     @o_player = Player.new("o")
+    @game_has_been_won = false
+    @winner = nil
+    @current_turn = @x_player
+    @draw = false
+
   end
 
   def check_win_condition
@@ -16,12 +21,48 @@ class Board
     winning_combos.each do |combo|
       
       if $board_state[combo[0]] == $board_state[combo[1]] && $board_state[combo[1]] == $board_state[combo[2]] && $board_state[combo[0]] != "_"
-        #puts "I detected a win"
-        return true
+        @game_has_been_won = true
+        if $board_state[combo[0]] == "x"
+          @winner = @x_player
+          puts "x wins"
+        else
+          @winner = @o_player
+          puts "o wins"
+        end
+        #puts @winner
       end
+    end
+
+    if !@game_has_been_won
+      count_for_draw = 0
+      $board_state.each_value do |square|
+        if square == "x" || square == "o"
+          count_for_draw += 1
+        end
+      end
+      if count_for_draw == 9
+        @draw = true
+        puts "its a draw!"
+      end
+    end
+  end
+
+  def game_loop
+    while @winner == nil && @draw == false
+      if @current_turn == @x_player
+        puts "x to move"
+        @x_player.make_move
+        @current_turn = @o_player
+      elsif @current_turn == @o_player
+        puts "o to move"
+        @o_player.make_move
+        @current_turn = @x_player
+      end
+      check_win_condition
 
     end
   end
+
 end
 
 class Player
@@ -30,19 +71,19 @@ class Player
     @move = move
   end
 
-  def make_move(position) # write a recursive to keep going until they give valid move
+  def make_move
+    position = gets
+    position = position.to_i
     if $board_state[position] == "_"
       $board_state[position] = move
     end
+    #puts $board_state
   end
 
 end
 
-
 board = Board.new
-board.x_player.make_move(4)
-board.x_player.make_move(5)
-board.x_player.make_move(6)
 
-puts board.check_win_condition
+board.game_loop
+
 
